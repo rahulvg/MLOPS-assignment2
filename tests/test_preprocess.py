a85c59f0b7f2811773d066491f21b6bf8e864ca3
GALGALI Rahul <RGI3@dsone.3ds.com>
2026-02-17 17:24:10 +0530

    Test image

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -2,6 +2,6 @@ import torch
 from src.preprocess import load_image
 
 def test_image_shape():
-    tensor = load_image("data/raw/cat/50.jpg")
+    tensor = load_image("50.jpg")
     assert isinstance(tensor, torch.Tensor)
     assert tensor.shape == (3,224,224)
